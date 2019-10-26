d3cc7aa6465ff01b5493b96bc6f9e628b3f794d8
wwei <429744781@qq.com>
2019-10-26 14:10:35 +0800

    代码提交

---
+++ src/test/java/com/ww/springcloudconfigserver/SpringcloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ww.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
